10b9ccf40feabbacca9a783e157766e80b6560c1
Linengyao <1547784788@qq.com>
2020-04-03 13:47:04 +0800

    diyiti

--- CannyFilter/CannyFilter.cpp
+++ CannyFilter/CannyFilter.cpp
@@ -2,10 +2,36 @@
 //
 
 #include <iostream>
-
+#include <opencv.hpp>
+using namespace cv;
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	VideoCapture cap(0);
+
+	while (1)
+	{
+		cv::Mat frame;
+		cv::Mat CannyMat1;
+		cv::Mat CannyMat2;
+		cv::Mat Edge_X;
+		cv::Mat Edge_Y;
+		cap >> frame;
+
+		Canny(frame, CannyMat1, 50, 150);
+		
+		//x,y方向微分
+		Sobel(frame, Edge_X, CV_16SC1, 1, 0, 3);
+		Sobel(frame, Edge_Y, CV_16SC1, 0, 1, 3);
+		Canny(Edge_X, Edge_Y, CannyMat2, 50, 150);
+
+		imshow("frame", frame);
+		imshow("CannyMat1", CannyMat1);
+		imshow("CannyMat2", CannyMat2);
+		waitKey(30);
+
+	}
+	 //std::cout << "Hello World!\n";
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
